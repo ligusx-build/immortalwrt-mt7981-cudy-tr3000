7f45e0cc41ebc9d88cb41013885ef65861646da7
ligusx-build <37890938+ligusx-build@users.noreply.github.com>
2026-07-18 08:47:17 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,10 +14,16 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> ./feeds.conf.default
+echo "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> ./feeds.conf.default
+
+# Copy custom local packages into OpenWrt tree so they are available during build
+if [ -d "$GITHUB_WORKSPACE/package/luci-compat-keep" ]; then
+  mkdir -p package
+  cp -r "$GITHUB_WORKSPACE/package/luci-compat-keep" package/
+fi
 
 git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
-git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
-git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
